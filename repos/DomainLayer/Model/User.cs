3632f2f7a996b2e6e54225973aa8158ff4d14b8a
Herbert L. Navarro <168047070+herbertDev12@users.noreply.github.com>
2025-01-18 13:54:20 -0500

    Onion structure + user Information
    Added the onion structure to the program and some user data in the User class within the Domain layer.

---
+++ repos/DomainLayer/Model/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DomainLayer.Model
+{
+    public class User
+    {
+        [Key]
+        public long userID { get; set; }
+        public string userName { get; set; }
+        public string userPhone  { get; set; }
+        public string userEmail { get; set; }
+        public string userAddress { get; set; }
+        public DateTime createDateTime { get; set; }
+        public DateTime updateDateTime { get; set; }
+    }
+}
